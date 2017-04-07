04f66513bb3a0e8f01c68ff937d20e81ee2cce65
DESKTOP-T5D7VJM\Adri <flash.ar79@gmail.com>
2017-04-07 18:10:23 -0400

    send base64 img to server save to DB send to other webservice

--- camera/models.py
+++ camera/models.py
@@ -1,3 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+#superuser:
+#   root
+#pass:
+#   toor1234
 # Create your models here.
+class photos(models.Model):
+    #user = models.ForeignKey(User,on_delete=models.CASCADE)
+    photo = models.CharField(max_length=1300000)
--- camera/views.py
+++ camera/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 import base64
 import re
+import requests
+import json
 from django.http import JsonResponse
 
 from django.views.decorators.csrf import csrf_exempt
-
+from camera.models import *
 
 # Create your views here.
 
@@ -13,12 +15,20 @@ def camera(request):
 
 @csrf_exempt
 def ajaxupload(request):
+    url = "http://localhost:8000/raspberry_rest/"
+    headers = {'content-type': 'application/json'}
     img64=request.POST['imgBase64']
-    #print(img64)
-    f = open("temp.png", "w")
-    f.write(img64)
-    f.close()
+    print(len(img64))
+    #f = open("temp.png", "w")
+    #f.write(img64)
+    #f.close()
     #ImageData = base64.b64decode(img64)
+    cam=photos()
+    cam.user=request.user
+    cam.photo=img64
+    cam.save()
+    data={'photo':img64}
+    r = requests.post(url, data=json.dumps(data), headers=headers)
     return JsonResponse({'saved':'ok'})
 
 
